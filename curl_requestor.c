e72e0321e65ef51efac31a599d338270a45b70d4
0xbadc0fee <sgc98944@protonmail.com>
2023-01-13 20:33:10 -0800

    x86 localhost test GOOD

--- curl_requestor.c
+++ curl_requestor.c
@@ -8,29 +8,17 @@
  */
 #include <stdio.h>
 #include <curl/curl.h>
-
-#define OK		0
-#define INIT_ERR	1
-#define REQ_ERR		2
-
-#define URL		"http://localhost:8000"
-
 int main(void)
 {
   CURL *curl;
   CURLcode res;
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, URL);
+    curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
     res = curl_easy_perform(curl);
-    if (res != CURLM_OK) {
-	    return REQ_ERR;
-    }
     /* always cleanup */
     curl_easy_cleanup(curl);
-  } else {
-	  return INIT_ERR;
   }
-  return OK;
+  return 0;
 }
 
